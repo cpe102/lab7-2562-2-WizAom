5d44170b5e3c8917eea910fe09da6a662e623c82
WizAom <Aom_ll2555@hotmail.com>
2020-01-21 15:55:52 +0700

    1st

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,8 +2,14 @@
 #include<iomanip>
 #include<string>
 using namespace std;
+string findGrade(float x){
+	if(x>=90) return "A";
+	else if(x>=75) return "B";
+	else if(x>=60) return "C";
+	else if(x>45) return "D";
+	else if(x<=345) return "F";
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +23,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,29 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int age,h,m;
+    string stt="",stt1="UNFRIEND",stt2="FRIEND",stt3="MARRIED",stt4="BEST FRIEND",stt5="ONE-NIGHT-STAND";
+    cout << "input Age :";
+    cin >> age;
+    if(age<=20){
+        cout << "input Hiegh :";
+        cin >> h;
+        if(h<160) stt=stt1;
+        else if (h<175) stt=stt2;
+             else{
+                cout << "input money :";
+                cin >> m;
+                if(m>69000000) stt=stt3;
+                else stt=stt5;
+            }
+
+    }else if(age<30){
+        cout << "input money :";
+        cin >> m;
+        if(m>10000000) stt=stt4;
+        else stt=stt1;
+    }else stt=stt1;
+    cout << stt;
+    return 0;
+}
